c60bc61c4f02f152c2085724185553b5969babd2
hacks-KS <b1013027@gamil.com>
2017-06-09 21:11:34 +0100

    Add: component with redux

--- src/provider.jsx
+++ src/provider.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
-import Game from './containers/Game.jsx';
+import Container from './containers/container';
 import configureStore from './store/store';
 
 const store = configureStore();
@@ -9,7 +9,7 @@ const contentEl = document.getElementById('root');
 
 ReactDOM.render(
     <Provider store={store}>
-        <Game />
+        <Container />
     </Provider>,
     contentEl
 );
