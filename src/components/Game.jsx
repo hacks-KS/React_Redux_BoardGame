be40e057b3c5d83afc3a828cf16caa2defe2c0d8
hacks-KS <b1013027@gamil.com>
2017-06-09 21:11:34 +0100

    Add: component with redux

---
+++ src/components/Game.jsx
@@ -0,0 +1,77 @@
+import React from 'react';
+import Board from './Board.jsx';
+
+export default class Game extends React.Component {
+
+  handleClick(i) {
+    const history = this.props.history.slice(0, this.props.stepNumber + 1);
+    const current = history[history.length - 1];
+    const squares = current.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = this.props.xIsNext ? "X" : "O";
+    this.props.turn_change(history, squares);
+  }
+
+  jumpTo(step) {
+    this.props.jump_history(step);
+  }
+
+  render() {
+    const history = this.props.history;
+    const current = history[this.props.stepNumber];
+    const winner = calculateWinner(current.squares);
+
+    const moves = history.map((step, move) => {
+      const desc = move ? "Move #" + move : "Game start";
+      return (
+        <li key={move}>
+          <a href="#" onClick={() => this.jumpTo(move)}>{desc}</a>
+        </li>
+      );
+    });
+
+    let status;
+    if (winner) {
+      status = "Winner: " + winner;
+    } else {
+      status = "Next player: " + (this.props.xIsNext ? "X" : "O");
+    }
+
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board
+            squares={current.squares}
+            onClick={i => this.handleClick(i)}
+          />
+        </div>
+        <div className="game-info">
+          <div>{status}</div>
+          <ol>{moves}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
--- src/components/square.jsx
+++ src/components/square.jsx
@@ -1,24 +1,9 @@
-import React from 'react'
-import marked from 'marked';
+import React from 'react';
 
-//コメント表示の一番最下層のコンポーネント
-//このコンポーネントでコメント表示のDOMを作成する
-export default class Comment extends React.Component{
-  //マークダウン記法でコメントを書けるようにする関数
-  rawMarkup() {
-    let rawMarkupHtml = marked(this.props.children.toString(), {sanitize: true});
-    return { __html: rawMarkupHtml };
-  }
-
-  //コメント表示のDOMを作成する
-  render() {
-    return (
-      <div className="comment">
-        <h2 className="commentAuthor">
-          {this.props.author}
-        </h2>
-        <span dangerouslySetInnerHTML={this.rawMarkup()} />
-      </div>
-    );
-  }
-};
+export default function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
+}
